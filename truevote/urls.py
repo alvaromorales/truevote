902d944bec873a932943825d8ba338a3df2539f7
unknown <Shini@.(none)>
2013-04-11 13:17:36 -0400

    Removed deprecated module from urls.py

--- truevote/urls.py
+++ truevote/urls.py
@@ -1,18 +1,12 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^$', direct_to_template, {
-            'template':'login.html'
-            }),
-                       url(r'^audit$', direct_to_template, {
-            'template':'index.html'
-            }),
-                       url(r'^warden$', direct_to_template, {
-            'template':'warden.html'
-            }),
+                       url(r'^$', TemplateView.as_view(template_name='login.html')),
+                       url(r'^audit$', TemplateView.as_view(template_name='index.html')),
+                       url(r'^warden$', TemplateView.as_view(template_name='warden.html')),
 )
