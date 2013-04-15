40ac17456a7091994484a8e4043cae3c446cf257
Ayesha Bose <aybose@mit.edu>
2013-04-15 18:38:58 -0400

    Added warden files

--- truevote/urls.py
+++ truevote/urls.py
@@ -7,6 +7,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^$', TemplateView.as_view(template_name='login.html')),
-                       url(r'^audit$', TemplateView.as_view(template_name='index.html')),
-                       url(r'^warden$', TemplateView.as_view(template_name='warden.html')),
+                       url(r'^audit/$', TemplateView.as_view(template_name='index.html')),
+                       url(r'^warden/$', TemplateView.as_view(template_name='warden.html')),
+                       url(r'^warden/createballot$', TemplateView.as_view(template_name='createballot.html')),
+                       url(r'^warden/results$', TemplateView.as_view(template_name='results.html')),
 )
