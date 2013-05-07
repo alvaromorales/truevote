f83bef0247e7f85aa223f7e743a2cef69daf31e0
Rachael Naphtal <rnaphtal@dhcp-18-111-26-239.dyn.mit.edu>
2013-05-07 02:25:22 -0400

    finished tutorial on main page

--- truevote/static/js/tutorial_vote-count.js
+++ truevote/static/js/tutorial_vote-count.js
@@ -1,5 +1,15 @@
 $(function() {
+	var tutorialStep=0;
 	updateButtons();
+	bootstro.on_step(
+          function() {
+          	tutorialStep+=1;
+          	if (tutorialStep==7){
+          		$('#fixMistakeBtn').attr('enabled','enabled');
+				
+				
+          	}}
+        );
 });
 
 var updateButtons = function() {
@@ -18,26 +28,21 @@ var displayVoteCountButtons = function(race) {
 	var buttonsDiv = $('.countButtons');
 	
 
-	$('#voteCountDiv').css('width',$(window).width()-350);
-	
 	var width = $(window).width();
-	
+	buttonsDiv.css("width", width-300);
 	buttonsDiv.html(''); //clear buttons
 
 	if (race) {
 		var candidates = race.candidates;
-		var candidateName = $("<h1>" + race.name + "</h1>");
-		candidateName.css('font-family','\'Istok Web\', sans-serif');
-		candidateName.css('color','#5F0000');
-		candidateName.css('font-weight','bold');
-		candidateName.css('position','absolute');
-		candidateName.css('top','42.5%');
-		candidateName.css('right','18%');
-		candidateName.css('word-wrap','break-word');
-		candidateName.css('width','35%');
-		candidateName.css('text-align','center');
+		var raceName = $("<h1>" + race.name + "</h1>");
+		raceName.css('position','absolute');
+		raceName.css('top','42.5%');
+		raceName.css('right','25%');
+		raceName.css('word-wrap','break-word');
+		raceName.css('width','45%');
+		raceName.css('text-align','center');
 
-		buttonsDiv.append(candidateName);
+		buttonsDiv.append(raceName);
 
 		var numOther = 0;
 		for (var i = 0;i<candidates.length;i++) {
@@ -53,21 +58,25 @@ var displayVoteCountButtons = function(race) {
 			var candidate = $("<input type='button' class='raceBtn btn btn-info' value='" + c.name + "'></input>");
 			candidate.css('position','absolute');
 			if (c.party == republicanParty) {
+				candidate = $("<input type='button' class='raceBtn btn btn-info bootstro initialTutorial' data-bootstro-step='2' data-bootstro-content='The Republican candidate will always appear on the Bottom.' data-bootsro-title='Welcome to TrueVote!' data-bootstro-placement='top' value='" + c.name + "'></input>");
 				candidate.css('height','20%');
-				candidate.css('width','32.5%');
-				candidate.css('left','47.5%');
+				candidate.css('width','43%');
+				candidate.css('left','27%');
 				candidate.css('bottom','5%');
+				candidate.css('position','absolute');
 				candidate.addClass('btn-info-top');
 			} else if (c.party == democraticParty) {
+				candidate = $("<input type='button' class='raceBtn btn btn-info bootstro initialTutorial' data-bootstro-step='1' data-bootstro-content='The Democratic candidate will always appear on the Top.' data-bootsro-title='Welcome to TrueVote!' data-bootstro-placement='bottom' value='" + c.name + "'></input>");
 				candidate.css('top','5%');
 				candidate.css('height','20%');
-				candidate.css('width','32.5%');
-				candidate.css('left','47.5%');
+				candidate.css('width','43%');
+				candidate.css('left','27%');
+				candidate.css('position','absolute');
 				candidate.addClass('btn-info-top');
 			} else {
 				candidate.css('top',(15 + currentOther*(70.0/numOther + 2.5*(numOther-1))) +'%');
-				candidate.css('width','15%');
-				candidate.css('right','55%');
+				candidate.css('width','20%');
+				candidate.css('left','3.5%');
 
 				var height = Math.min((70.0/numOther - 2.5*(numOther-1)),32.5);
 				candidate.css('height', height + '%');
@@ -75,21 +84,31 @@ var displayVoteCountButtons = function(race) {
 			}
 			buttonsDiv.append(candidate);
 		}
+		var blankWriteInDiv = $("<div class='bootstro initialTutorial' data-bootstro-step='3' data-bootsro-title='Welcome to TrueVote!' data-bootstro-placement='left' data-bootstro-content='The buttons for blank and write-in will always appear in the same place on the screen.'></div>")
+		blankWriteInDiv.css('position','absolute');
+		blankWriteInDiv.css('right','6.5%');
+		blankWriteInDiv.css('top', '15%');
+		blankWriteInDiv.css('width', '20%');
+		blankWriteInDiv.css('height', '75%');
+		buttonsDiv.append(blankWriteInDiv);
+
 		var blank = $("<input type='button' class='raceBtn btn btn-info' value='Blank'></input>");
 		blank.css('position','absolute');
-		blank.css('right','2.5%');
-		blank.css('top', '15%');
-		blank.css('width', '15%');
-		blank.css('height', '32.5%');
-		buttonsDiv.append(blank);
+		blank.css('right','0%');
+		blank.css('top', '0%');
+		blank.css('width', '100%');
+		blank.css('height', '43.3%');
+		blankWriteInDiv.append(blank);
 
 		var writeIn = $("<input type='button' class='raceBtn btn btn-info' value='Write-In'></input>");
 		writeIn.css('position','absolute');
-		writeIn.css('right','2.5%');
-		writeIn.css('top', '52.5%');
-		writeIn.css('width', '15%');
-		writeIn.css('height', '32.5%');
-		buttonsDiv.append(writeIn);
+		writeIn.css('right','0%');
+		writeIn.css('top', '56.67%');
+		writeIn.css('width', '100%');
+		writeIn.css('height', '43.3%');
+		blankWriteInDiv.append(writeIn);
+
+		
 
 		$('.raceBtn').click(function(e) {
 			var winner = e.target.value;
@@ -100,7 +119,7 @@ var displayVoteCountButtons = function(race) {
 				previousRace.setWinner(winner);
 				updateSidebar(previousRace);
 			}
-			updateButtons();
+			//updateButtons();
 		});
 	}
 }
